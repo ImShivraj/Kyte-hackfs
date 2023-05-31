2f59b80a23915e60c56b540b81672998b6b5dd84
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2023-05-31 12:29:04 +0530

    plus button for post

--- src/components/UI/DashboardLayout.tsx
+++ src/components/UI/DashboardLayout.tsx
@@ -3,14 +3,20 @@ import Sidebar from "./Sidebar"
 import { useRouter } from "next/router"
 import clsx from "clsx"
 import SidebarMobile from "./SidebarMobile"
+import { BiPlus } from "react-icons/bi"
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
     const router = useRouter()
     return (
         // xl1450:w-[350px]
         <div className=" grid grid-cols-12 relative bg-red-30">
-            <div className="xl1450:col-span-2  md900:w-20 col-span-3 bg-pink-30 hidden md850:block ">
+            <div className="xl1450:col-span-2  md900:w-16 col-span-3 bg-pink-30 hidden md850:block ">
                 <Sidebar />
+            </div>
+            <div className=" transition ease-in-out active:scale-95 z-50 fixed bottom-24 right-5 w-auto md850:hidden block ">
+                <div className=" bg-lightGreen rounded-full p-4">
+                    <BiPlus size={"2.2em"} color="white" />
+                </div>
             </div>
             <div className="z-50 fixed bottom-0 w-full md850:hidden block ">
                 <SidebarMobile />
